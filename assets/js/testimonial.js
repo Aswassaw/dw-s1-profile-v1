bfc07b8a9b42c30aa16116c2a2b571f66b9403da
Andry Pebrianto <andrypeb227@gmail.com>
2023-08-18 14:57:41 +0700

    Menyelesaikan task day 8

--- assets/js/testimonial.js
+++ assets/js/testimonial.js
@@ -1,86 +1,136 @@
-class Testimonial {
-  #message = "";
-  #image = "";
+const testimonialData = [
+  {
+    author: "Hiita the Fire Charmer, Ablaze",
+    message:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam, deserunt perspiciatis nulla rerum blanditiis officia!",
+    image:
+      "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/43b9db00-2237-441d-b909-5171426db592/dfu1az0-7f76e045-d2ff-4cce-bbf6-3e361b1428ac.png/v1/fit/w_828,h_828,q_70,strp/hiita_the_fire_charmer__ablaze_by_thehungtd_dfu1az0-414w-2x.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9MTIwMCIsInBhdGgiOiJcL2ZcLzQzYjlkYjAwLTIyMzctNDQxZC1iOTA5LTUxNzE0MjZkYjU5MlwvZGZ1MWF6MC03Zjc2ZTA0NS1kMmZmLTRjY2UtYmJmNi0zZTM2MWIxNDI4YWMucG5nIiwid2lkdGgiOiI8PTEyMDAifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6aW1hZ2Uub3BlcmF0aW9ucyJdfQ.n_T7h7lNjlCEZ893hc9Hp4jVdIbwsaE0ngatSOQNIEU",
+    rating: 2,
+  },
+  {
+    author: "Wynn the Wind Charmer, Verdant",
+    message:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus deleniti porro labore, voluptatem itaque molestiae.",
+    image:
+      "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/43b9db00-2237-441d-b909-5171426db592/dfu1b0t-abe11fbf-8689-464d-bfc5-3c365f31999b.png/v1/fit/w_828,h_828,q_70,strp/wynn_the_wind_charmer__verdant_by_thehungtd_dfu1b0t-414w-2x.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9MTIwMCIsInBhdGgiOiJcL2ZcLzQzYjlkYjAwLTIyMzctNDQxZC1iOTA5LTUxNzE0MjZkYjU5MlwvZGZ1MWIwdC1hYmUxMWZiZi04Njg5LTQ2NGQtYmZjNS0zYzM2NWYzMTk5OWIucG5nIiwid2lkdGgiOiI8PTEyMDAifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6aW1hZ2Uub3BlcmF0aW9ucyJdfQ.4GaV0_41i_uQXey_tegHjhHibJOaCiqCX_nHVejpb2w",
+    rating: 5,
+  },
+  {
+    author: "Eria the Water Charmer, Gentle",
+    message:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium illum nostrum impedit culpa perferendis vero!",
+    image:
+      "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/43b9db00-2237-441d-b909-5171426db592/dfu1aub-461c9d77-78c9-4826-b848-373075ed8910.png/v1/fit/w_828,h_828,q_70,strp/eria_the_water_charmer__gentle_by_thehungtd_dfu1aub-414w-2x.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9MTIwMCIsInBhdGgiOiJcL2ZcLzQzYjlkYjAwLTIyMzctNDQxZC1iOTA5LTUxNzE0MjZkYjU5MlwvZGZ1MWF1Yi00NjFjOWQ3Ny03OGM5LTQ4MjYtYjg0OC0zNzMwNzVlZDg5MTAucG5nIiwid2lkdGgiOiI8PTEyMDAifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6aW1hZ2Uub3BlcmF0aW9ucyJdfQ.jvLiUC67RdJC4NquuAEjT4FlQ6xLeePdwYHuiE2rPoQ",
+    rating: 5,
+  },
+  {
+    author: "Aussa the Earth Charmer, Immovable",
+    message:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis cupiditate deleniti voluptatum veniam nam harum!",
+    image:
+      "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/d65b5b43-ba00-4e33-8490-41ee63b33721/dejbvrd-59a5d767-19fc-4a7c-8e77-a350b716c02c.jpg/v1/fit/w_828,h_828,q_70,strp/aussa_the_earth_charmer__immovable_by_mrcat95_dejbvrd-414w-2x.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9MTIwMCIsInBhdGgiOiJcL2ZcL2Q2NWI1YjQzLWJhMDAtNGUzMy04NDkwLTQxZWU2M2IzMzcyMVwvZGVqYnZyZC01OWE1ZDc2Ny0xOWZjLTRhN2MtOGU3Ny1hMzUwYjcxNmMwMmMuanBnIiwid2lkdGgiOiI8PTEyMDAifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6aW1hZ2Uub3BlcmF0aW9ucyJdfQ.UPiqNEZIXJC-Oqsl6JGaGk7s_W3nP8iaxNfgshIc7k0",
+    rating: 2,
+  },
+  {
+    author: "Lyna the Light Charmer, Lustrous",
+    message:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita repellendus unde voluptatem, nemo temporibus laudantium.",
+    image:
+      "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/43b9db00-2237-441d-b909-5171426db592/dfu1azg-65ae6078-56e5-4f75-bdee-74d8d2f73677.png/v1/fit/w_828,h_828,q_70,strp/lyna_the_light_charmer__lustrous_by_thehungtd_dfu1azg-414w-2x.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9MTIwMCIsInBhdGgiOiJcL2ZcLzQzYjlkYjAwLTIyMzctNDQxZC1iOTA5LTUxNzE0MjZkYjU5MlwvZGZ1MWF6Zy02NWFlNjA3OC01NmU1LTRmNzUtYmRlZS03NGQ4ZDJmNzM2NzcucG5nIiwid2lkdGgiOiI8PTEyMDAifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6aW1hZ2Uub3BlcmF0aW9ucyJdfQ.94MLOEK8-5FGieoNLRm0dxtZtU5u1nh0VxczjtprYGk",
+    rating: 4,
+  },
+  {
+    author: "Dharc the Dark Charmer, Umbral",
+    message:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repellat cupiditate ipsum, vitae asperiores reiciendis fuga.",
+    image:
+      "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/d65b5b43-ba00-4e33-8490-41ee63b33721/despmt7-e1917862-427a-429f-9485-71d8a5c5c670.png/v1/fit/w_828,h_828/dharc_the_dark_charmer__umbral_by_mrcat95_despmt7-414w-2x.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9MTIwMCIsInBhdGgiOiJcL2ZcL2Q2NWI1YjQzLWJhMDAtNGUzMy04NDkwLTQxZWU2M2IzMzcyMVwvZGVzcG10Ny1lMTkxNzg2Mi00MjdhLTQyOWYtOTQ4NS03MWQ4YTVjNWM2NzAucG5nIiwid2lkdGgiOiI8PTEyMDAifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6aW1hZ2Uub3BlcmF0aW9ucyJdfQ.righ18mUEBQwAxf8xDUl_SnObYyv2K4fNksSJfXLBQo",
+    rating: 1,
+  },
+];
 
-  constructor(message, image) {
-    this.#message = message;
-    this.#image = image;
-  }
+function allTestimonials() {
+  let testimonialHTML = "";
 
-  get message() {
-    return this.#message;
-  }
+  testimonialData.forEach(function (item) {
+    let starElement = "";
+    let filledStar = 0;
 
-  get image() {
-    return this.#image;
-  }
+    for (let i = 0; i < 5; i++) {
+      if (filledStar >= item.rating) {
+        starElement += `<i class="fa-regular fa-star"></i>`;
+      } else {
+        starElement += `<i class="fa-solid fa-star"></i>`;
+      }
 
-  get author() {
-    throw new Error("getAuthor() method must be implemented.");
-  }
+      filledStar++;
+    }
 
-  get testimonialHTML() {
-    return `
-      <div class="testimonial-card">
-        <img class="testimonial-image" src="${this.image}" alt="Gambar alt" width="100%">
-        <p class="testimonial-message">${this.message}</p>
-        <p class="testimonial-author">
-          - ${this.author}
-        </p>
-      </div>
-    `;
-  }
+    testimonialHTML += `
+    <div class="testimonial-card">
+      <div class="rating-value">${starElement}</div>
+      <img class="testimonial-image" src="${item.image}" alt="Gambar alt" width="100%">
+      <p class="testimonial-message">${item.message}</p>
+      <p class="testimonial-author">
+        - ${item.author}
+      </p>
+    </div>`;
+  });
+
+  document.getElementById("testimonial").innerHTML = testimonialHTML;
 }
 
-class AuthorTestimonial extends Testimonial {
-  #author = "";
+allTestimonials();
 
-  constructor(author, message, image) {
-    super(message, image);
-    this.#author = author;
+function filterTestimonial(ratingVal) {
+  // select all other rating button
+  const allRatingButton = document.querySelectorAll(".rating-list span");
+
+  // remove class active from element
+  allRatingButton.forEach((e, index) => {
+    if (index === ratingVal) {
+      e.classList.add("active");
+    } else {
+      e.classList.remove("active");
+    }
+  });
+
+  const testimonialFilter = testimonialData.filter((testi) => {
+    if (ratingVal === 0) {
+      return true;
+    }
+    return testi.rating === ratingVal;
+  });
+
+  let testimonialHTML = "";
+  if (testimonialFilter.length) {
+    testimonialFilter.forEach(function (item) {
+      let starElement = "";
+      let filledStar = 0;
+  
+      for (let i = 0; i < 5; i++) {
+        if (filledStar >= item.rating) {
+          starElement += `<i class="fa-regular fa-star"></i>`;
+        } else {
+          starElement += `<i class="fa-solid fa-star"></i>`;
+        }
+  
+        filledStar++;
+      }
+
+      testimonialHTML += `
+        <div class="testimonial-card">
+          <div class="rating-value">${starElement}</div>
+          <img class="testimonial-image" src="${item.image}" alt="Gambar alt" width="100%">
+          <p class="testimonial-message">${item.message}</p>
+          <p class="testimonial-author">
+            - ${item.author}
+          </p>
+        </div>`;
+    });
+  } else {
+    testimonialHTML += `<h1>Data Not Found!</h1>`;
   }
 
-  get author() {
-    return this.#author;
-  }
+  document.getElementById("testimonial").innerHTML = testimonialHTML;
 }
-
-class CompanyTestimonial extends Testimonial {
-  #company = "";
-
-  constructor(author, message, image) {
-    super(message, image);
-    this.#company = author;
-  }
-
-  get author() {
-    return this.#company + " Company";
-  }
-}
-
-const testimonial1 = new AuthorTestimonial(
-  "Slifer The Sky Dragon",
-  "Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus, perspiciatis.",
-  "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/c977eabb-4db6-4ef1-9f0c-1fe00e997eaa/dauamsg-81b98799-0fb0-475d-9138-8d4be8c927a3.png/v1/fill/w_894,h_894,q_70,strp/slifer_the_sky_dragon_1080p_by_yugi_master_dauamsg-pre.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9MTAyNCIsInBhdGgiOiJcL2ZcL2M5NzdlYWJiLTRkYjYtNGVmMS05ZjBjLTFmZTAwZTk5N2VhYVwvZGF1YW1zZy04MWI5ODc5OS0wZmIwLTQ3NWQtOTEzOC04ZDRiZThjOTI3YTMucG5nIiwid2lkdGgiOiI8PTEwMjQifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6aW1hZ2Uub3BlcmF0aW9ucyJdfQ.dwt4tSvVCOG0I17wRYKjC4JpOgYS6mnWaAmR7tIsmA0"
-);
-
-const testimonial2 = new AuthorTestimonial(
-  "Obelish The Tormentor",
-  "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Pariatur, quam.",
-  "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/c977eabb-4db6-4ef1-9f0c-1fe00e997eaa/dauamv6-6128c036-3ef5-4265-bd46-15431aa3d0b3.png/v1/fill/w_894,h_894,q_70,strp/obelisk_the_tormentor_1080p_by_yugi_master_dauamv6-pre.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9MTAyNCIsInBhdGgiOiJcL2ZcL2M5NzdlYWJiLTRkYjYtNGVmMS05ZjBjLTFmZTAwZTk5N2VhYVwvZGF1YW12Ni02MTI4YzAzNi0zZWY1LTQyNjUtYmQ0Ni0xNTQzMWFhM2QwYjMucG5nIiwid2lkdGgiOiI8PTEwMjQifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6aW1hZ2Uub3BlcmF0aW9ucyJdfQ.gwYrAAexfTifjr84WhYS3xPGcMiD9sXAhR9mLw87y4Q"
-);
-
-const testimonial3 = new AuthorTestimonial(
-  "The Winged Dragon Of Ra",
-  "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laudantium, itaque.",
-  "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/c977eabb-4db6-4ef1-9f0c-1fe00e997eaa/dauamxf-1ce85694-1433-48d2-a12b-c439989e3439.png/v1/fill/w_894,h_894,q_70,strp/the_winged_dragon_of_ra_1080p_by_yugi_master_dauamxf-pre.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9MTAyNCIsInBhdGgiOiJcL2ZcL2M5NzdlYWJiLTRkYjYtNGVmMS05ZjBjLTFmZTAwZTk5N2VhYVwvZGF1YW14Zi0xY2U4NTY5NC0xNDMzLTQ4ZDItYTEyYi1jNDM5OTg5ZTM0MzkucG5nIiwid2lkdGgiOiI8PTEwMjQifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6aW1hZ2Uub3BlcmF0aW9ucyJdfQ.cmu0DD65lXQsTifIRQVVA3_LkP_Oh9M3tpmzg28KGPA"
-);
-
-let testimonialData = [testimonial1, testimonial2, testimonial3];
-let testimonialHTML = "";
-
-for (let i = 0; i < testimonialData.length; i++) {
-  testimonialHTML += testimonialData[i].testimonialHTML;
-}
-
-document.getElementById("testimonial").innerHTML = testimonialHTML;
