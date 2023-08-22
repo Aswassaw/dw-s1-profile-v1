83fa19f0594623dbe6e9cae59dc02df8c6709e96
Andry Pebrianto <andrypeb227@gmail.com>
2023-08-22 14:06:30 +0700

    Menyelesaikan task day 8

--- assets/js/testimonial.js
+++ assets/js/testimonial.js
@@ -1,124 +1,103 @@
-const testimonialData = [
-  {
-    author: "Hiita the Fire Charmer, Ablaze",
-    message:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam, deserunt perspiciatis nulla rerum blanditiis officia!",
-    image:
-      "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/43b9db00-2237-441d-b909-5171426db592/dfu1az0-7f76e045-d2ff-4cce-bbf6-3e361b1428ac.png/v1/fit/w_828,h_828,q_70,strp/hiita_the_fire_charmer__ablaze_by_thehungtd_dfu1az0-414w-2x.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9MTIwMCIsInBhdGgiOiJcL2ZcLzQzYjlkYjAwLTIyMzctNDQxZC1iOTA5LTUxNzE0MjZkYjU5MlwvZGZ1MWF6MC03Zjc2ZTA0NS1kMmZmLTRjY2UtYmJmNi0zZTM2MWIxNDI4YWMucG5nIiwid2lkdGgiOiI8PTEyMDAifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6aW1hZ2Uub3BlcmF0aW9ucyJdfQ.n_T7h7lNjlCEZ893hc9Hp4jVdIbwsaE0ngatSOQNIEU",
-    rating: 2,
-  },
-  {
-    author: "Wynn the Wind Charmer, Verdant",
-    message:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus deleniti porro labore, voluptatem itaque molestiae.",
-    image:
-      "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/43b9db00-2237-441d-b909-5171426db592/dfu1b0t-abe11fbf-8689-464d-bfc5-3c365f31999b.png/v1/fit/w_828,h_828,q_70,strp/wynn_the_wind_charmer__verdant_by_thehungtd_dfu1b0t-414w-2x.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9MTIwMCIsInBhdGgiOiJcL2ZcLzQzYjlkYjAwLTIyMzctNDQxZC1iOTA5LTUxNzE0MjZkYjU5MlwvZGZ1MWIwdC1hYmUxMWZiZi04Njg5LTQ2NGQtYmZjNS0zYzM2NWYzMTk5OWIucG5nIiwid2lkdGgiOiI8PTEyMDAifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6aW1hZ2Uub3BlcmF0aW9ucyJdfQ.4GaV0_41i_uQXey_tegHjhHibJOaCiqCX_nHVejpb2w",
-    rating: 5,
-  },
-  {
-    author: "Eria the Water Charmer, Gentle",
-    message:
-      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium illum nostrum impedit culpa perferendis vero!",
-    image:
-      "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/43b9db00-2237-441d-b909-5171426db592/dfu1aub-461c9d77-78c9-4826-b848-373075ed8910.png/v1/fit/w_828,h_828,q_70,strp/eria_the_water_charmer__gentle_by_thehungtd_dfu1aub-414w-2x.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9MTIwMCIsInBhdGgiOiJcL2ZcLzQzYjlkYjAwLTIyMzctNDQxZC1iOTA5LTUxNzE0MjZkYjU5MlwvZGZ1MWF1Yi00NjFjOWQ3Ny03OGM5LTQ4MjYtYjg0OC0zNzMwNzVlZDg5MTAucG5nIiwid2lkdGgiOiI8PTEyMDAifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6aW1hZ2Uub3BlcmF0aW9ucyJdfQ.jvLiUC67RdJC4NquuAEjT4FlQ6xLeePdwYHuiE2rPoQ",
-    rating: 5,
-  },
-  {
-    author: "Aussa the Earth Charmer, Immovable",
-    message:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis cupiditate deleniti voluptatum veniam nam harum!",
-    image:
-      "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/d65b5b43-ba00-4e33-8490-41ee63b33721/dejbvrd-59a5d767-19fc-4a7c-8e77-a350b716c02c.jpg/v1/fit/w_828,h_828,q_70,strp/aussa_the_earth_charmer__immovable_by_mrcat95_dejbvrd-414w-2x.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9MTIwMCIsInBhdGgiOiJcL2ZcL2Q2NWI1YjQzLWJhMDAtNGUzMy04NDkwLTQxZWU2M2IzMzcyMVwvZGVqYnZyZC01OWE1ZDc2Ny0xOWZjLTRhN2MtOGU3Ny1hMzUwYjcxNmMwMmMuanBnIiwid2lkdGgiOiI8PTEyMDAifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6aW1hZ2Uub3BlcmF0aW9ucyJdfQ.UPiqNEZIXJC-Oqsl6JGaGk7s_W3nP8iaxNfgshIc7k0",
-    rating: 2,
-  },
-  {
-    author: "Lyna the Light Charmer, Lustrous",
-    message:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita repellendus unde voluptatem, nemo temporibus laudantium.",
-    image:
-      "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/43b9db00-2237-441d-b909-5171426db592/dfu1azg-65ae6078-56e5-4f75-bdee-74d8d2f73677.png/v1/fit/w_828,h_828,q_70,strp/lyna_the_light_charmer__lustrous_by_thehungtd_dfu1azg-414w-2x.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9MTIwMCIsInBhdGgiOiJcL2ZcLzQzYjlkYjAwLTIyMzctNDQxZC1iOTA5LTUxNzE0MjZkYjU5MlwvZGZ1MWF6Zy02NWFlNjA3OC01NmU1LTRmNzUtYmRlZS03NGQ4ZDJmNzM2NzcucG5nIiwid2lkdGgiOiI8PTEyMDAifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6aW1hZ2Uub3BlcmF0aW9ucyJdfQ.94MLOEK8-5FGieoNLRm0dxtZtU5u1nh0VxczjtprYGk",
-    rating: 4,
-  },
-  {
-    author: "Dharc the Dark Charmer, Gloomy",
-    message:
-      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repellat cupiditate ipsum, vitae asperiores reiciendis fuga.",
-    image:
-      "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/d65b5b43-ba00-4e33-8490-41ee63b33721/despmt7-e1917862-427a-429f-9485-71d8a5c5c670.png/v1/fit/w_828,h_828/dharc_the_dark_charmer__umbral_by_mrcat95_despmt7-414w-2x.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9MTIwMCIsInBhdGgiOiJcL2ZcL2Q2NWI1YjQzLWJhMDAtNGUzMy04NDkwLTQxZWU2M2IzMzcyMVwvZGVzcG10Ny1lMTkxNzg2Mi00MjdhLTQyOWYtOTQ4NS03MWQ4YTVjNWM2NzAucG5nIiwid2lkdGgiOiI8PTEyMDAifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6aW1hZ2Uub3BlcmF0aW9ucyJdfQ.righ18mUEBQwAxf8xDUl_SnObYyv2K4fNksSJfXLBQo",
-    rating: 1,
-  },
-];
+let testimonialData = [];
 
-function allTestimonials() {
-  let testimonialHTML = "";
+// fetch data from api
+const fetchTestimonial = new Promise((resolve, reject) => {
+  const xhr = new XMLHttpRequest();
 
-  testimonialData.forEach(function (item) {
-    let starElement = "";
-    let filledStar = 0;
-
-    for (let i = 0; i < 5; i++) {
-      if (filledStar >= item.rating) {
-        starElement += `<i class="fa-regular fa-star"></i>`;
-      } else {
-        starElement += `<i class="fa-solid fa-star"></i>`;
-      }
-
-      filledStar++;
-    }
-
-    testimonialHTML += `
-    <div class="testimonial-card">
-      <div class="rating-value">${starElement}</div>
-      <img class="testimonial-image" src="${item.image}" alt="Gambar alt" width="100%">
-      <p class="testimonial-message">${item.message}</p>
-      <p class="testimonial-author">
-        - ${item.author}
-      </p>
-    </div>`;
-  });
-
-  document.getElementById("testimonial").innerHTML = testimonialHTML;
-}
-
-allTestimonials();
-
-function filterTestimonial(ratingVal) {
-  // select all other rating button
-  const allRatingButton = document.querySelectorAll(".rating-list span");
-
-  // remove class active from element
-  allRatingButton.forEach((e, index) => {
-    if (index === ratingVal) {
-      e.classList.add("active");
+  xhr.open("GET", "https://api.npoint.io/a06208748f8723eb6c9b", true);
+  xhr.onload = function () {
+    if (xhr.status == 200) {
+      resolve(JSON.parse(xhr.response));
     } else {
-      e.classList.remove("active");
+      reject("Terjadi masalah saat sedang mengambil data!");
     }
-  });
+  };
+  xhr.onerror = function () {
+    reject("Network Error");
+  };
 
-  const testimonialFilter = testimonialData.filter((testi) => {
-    if (ratingVal === 0) {
-      return true;
-    }
-    return testi.rating === ratingVal;
-  });
+  xhr.send();
+});
 
-  let testimonialHTML = "";
-  if (testimonialFilter.length) {
-    testimonialFilter.forEach(function (item) {
+// print html element to web on first load
+async function allTestimonials() {
+  try {
+    testimonialData = await fetchTestimonial;
+
+    let testimonialHTML = "";
+    testimonialData.forEach(function (item) {
       let starElement = "";
       let filledStar = 0;
-  
+
       for (let i = 0; i < 5; i++) {
         if (filledStar >= item.rating) {
           starElement += `<i class="fa-regular fa-star"></i>`;
         } else {
           starElement += `<i class="fa-solid fa-star"></i>`;
         }
-  
+
         filledStar++;
       }
 
       testimonialHTML += `
+      <div class="testimonial-card">
+        <div class="rating-value">${starElement}</div>
+        <img class="testimonial-image" src="${item.image}" alt="Gambar alt" width="100%">
+        <p class="testimonial-message">${item.message}</p>
+        <p class="testimonial-author">
+          - ${item.author}
+        </p>
+      </div>`;
+    });
+
+    document.getElementById("testimonial").innerHTML = testimonialHTML;
+  } catch (error) {
+    console.log(error);
+    alert(error.message);
+  }
+}
+allTestimonials();
+
+// filter data
+async function filterTestimonial(ratingVal) {
+  try {
+    // select all other rating button
+    const allRatingButton = document.querySelectorAll(".rating-list span");
+
+    // remove class active from element
+    allRatingButton.forEach((e, index) => {
+      if (index === ratingVal) {
+        e.classList.add("active");
+      } else {
+        e.classList.remove("active");
+      }
+    });
+
+    testimonialData = await fetchTestimonial;
+    const testimonialFilter = testimonialData.filter((testi) => {
+      if (ratingVal === 0) {
+        return true;
+      }
+      return testi.rating === ratingVal;
+    });
+
+    let testimonialHTML = "";
+    if (testimonialFilter.length) {
+      testimonialFilter.forEach(function (item) {
+        let starElement = "";
+        let filledStar = 0;
+
+        for (let i = 0; i < 5; i++) {
+          if (filledStar >= item.rating) {
+            starElement += `<i class="fa-regular fa-star"></i>`;
+          } else {
+            starElement += `<i class="fa-solid fa-star"></i>`;
+          }
+
+          filledStar++;
+        }
+
+        testimonialHTML += `
         <div class="testimonial-card">
           <div class="rating-value">${starElement}</div>
           <img class="testimonial-image" src="${item.image}" alt="Gambar alt" width="100%">
@@ -127,10 +106,14 @@ function filterTestimonial(ratingVal) {
             - ${item.author}
           </p>
         </div>`;
-    });
-  } else {
-    testimonialHTML += `<h1>Data Not Found!</h1>`;
-  }
+      });
+    } else {
+      testimonialHTML += `<h1>Data Not Found!</h1>`;
+    }
 
-  document.getElementById("testimonial").innerHTML = testimonialHTML;
+    document.getElementById("testimonial").innerHTML = testimonialHTML;
+  } catch (error) {
+    console.log(error);
+    alert(error.message);
+  }
 }
